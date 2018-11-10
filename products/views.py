4775cc93cd9c815b6b3f1db5e4e9c22b60e80a25
Matheus Roberto <matheus456456@gmail.com>
2018-11-10 15:32:32 -0200

    Adding product create functionality
    Signed-off-by: Matheus Roberto <matheus456456@gmail.com>
    Co-Authored-By: mateuso97 <mat97_@hotmail.com>
    Co-authored-by: Eduardojvr  <eduardojvr10@gmail.com>

--- products/views.py
+++ products/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import redirect
 def list_products(request):
     products = Product.objects.all()
     return render(request, 'products.html', {"products": products})
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return render(request,  "products.html")
+    else:
+        return render(request, 'productCreate.html', {"form": form, "method": "Criar"})
