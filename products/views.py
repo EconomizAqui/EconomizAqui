f6cb55e68872427795615319a4eb207b983261ea
Matheus Roberto <matheus456456@gmail.com>
2018-11-10 15:35:16 -0200

    Adding product update and delete functionality
    Signed-off-by: Matheus Roberto <matheus456456@gmail.com>
    Co-Authored-By: mateuso97 <mat97_@hotmail.com>
    Co-authored-by: Eduardojvr  <eduardojvr10@gmail.com>

--- products/views.py
+++ products/views.py
@@ -14,3 +14,15 @@ def create_product(request):
         return render(request,  "products.html")
     else:
         return render(request, 'productCreate.html', {"form": form, "method": "Criar"})
+
+def update_product(request, id):
+    product = Product.objects.get(id=id)
+    form = ProductForm(request.POST or None, instance=product)
+    if form.is_valid():
+        form.save()
+        return render(request, 'productCreate.html', {"form": form, "method": "Atualizar"})
+    else:
+        return render(request, 'productCreate.html', {"form": form, "method": "Atualizar"})
+
+def delete_product(request):
+    return render(request, 'delete_product.html')
