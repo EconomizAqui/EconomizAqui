deb6b68e901e31b7544564d7462f41126c865dbc
Eduardojvr <eduardojvr10@gmail.com>
2018-11-21 00:14:15 -0200

    Adding observer notify user login
    Co-Authored-By: mateuso97 <mat97_@hotmail.com>
    Co-authored-by: matheus  <matheus456456@gmail.com>

--- users/services.py
+++ users/services.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 import abc
-
+import smtplib
+from email.mime.text import MIMEText
 
 class EventHandler(metaclass=abc.ABCMeta):
     @abc.abstractmethod
@@ -10,17 +11,18 @@ class EventHandler(metaclass=abc.ABCMeta):
 
 class NotificationEmailSender(EventHandler):
     def update(self, user):
-        email = user.email
-        
-        sent = send_mail(
-            'Alerta de login',
-            'Registramos um novo login em sua conta no EconomizAqui. '
-            + 'Caso não tenha sido você, favor redefinir sua senha.',
-            'economizaqui@email.com',
-            [email],
-            fail_silently=False,
-        )
+        email = user.email 
+        msg = 'Alerta de login.Registramos um novo login em sua conta no EconomizAqui. Caso não tenha sido você, favor redefinir sua senha!'
+        self.notify(msg, email)
 
-        if (sent):
-            print('Email de alerta de login enviado')
-
+    def notify(self, texto, email):
+        email_to = ['noreplayfiscae@gmail.com']
+        subject = 'Login detectado'
+        mensagem = MIMEText(texto)
+        mensagem.set_charset('utf-8')
+        mensagem['Subject'] = subject
+        mail = smtplib.SMTP('smtp.gmail.com', 587)
+        mail.ehlo()
+        mail.starttls()
+        mail.login('noreplayfiscae@gmail.com', 'fiscaeunb')
+        mail.sendmail('noreplayfiscae@gmail.com', email, mensagem.as_string())
